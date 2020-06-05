3afff2d22101e1234cbfcab7eee8ab56072e7737
Jarrod Johnson <jjohnson@new-innov.com>
2020-06-05 08:53:59 -0400

    Add Test for Pulsar.Client

--- DotPulsarTest/Program.cs
+++ DotPulsarTest/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using DotPulsar;
 using DotPulsar.Abstractions;
 using DotPulsar.Extensions;
+using Pulsar.Client.Api;
+using PulsarClient = DotPulsar.PulsarClient;
 
 namespace DotPulsarTest
 {
@@ -13,33 +15,64 @@ namespace DotPulsarTest
     {
         static async Task Main(string[] args)
         {
+            CancellationTokenSource cts = new CancellationTokenSource();
             const string myTopic = "persistent://public/default/mytopic";
+            
 
-            await using var client = PulsarClient.Builder()
+            cts.Cancel();
+        }
+
+        static async Task UsePulsarClient(string topic, CancellationToken cancel)
+        {
+            var client = new PulsarClientBuilder()
+                .ServiceUrl("pulsar://pulsar:6650")
+                .Build();
+
+            var producer = await client.NewProducer()
+                .Topic(topic)
+                .CreateAsync();
+
+            var consumer = await client.NewConsumer()
+                .Topic(topic)
+                .SubscriptionName("sub")
+                .SubscribeAsync();
+
+            var messageId = await producer.SendAsync(Encoding.UTF8.GetBytes($"Sent from C# at '{DateTime.Now}'"));
+            Console.WriteLine($"MessageId is: '{messageId}'");
+            
+            var message = await consumer.ReceiveAsync();
+            Console.WriteLine($"Received: {Encoding.UTF8.GetString(message.Data)}");
+
+            await consumer.AcknowledgeAsync(message.MessageId);
+        }
+
+        static async Task UseDotPulsar(string topic, CancellationToken cancel)
+        {
+            await using var client = PulsarClient.Builder().ExceptionHandler(async context =>
+                {
+                    Console.WriteLine(context.Exception?.ToString());
+                })
                 .ServiceUrl(new Uri("pulsar://pulsar:6650"))
-                .Build(); //Connecting to pulsar://localhost:6650
+                .Build();
 
             var producer = client.NewProducer()
-                .Topic(myTopic)
+                .Topic(topic)
                 .Create();
+            
+            Monitor(producer, cancel);
 
-            CancellationTokenSource cts = new CancellationTokenSource();
-
-            Monitor(producer, cts.Token);
-
-            _ = await producer.Send(Encoding.UTF8.GetBytes("Hello World"));
+            _ = await producer.Send(Encoding.UTF8.GetBytes("Hello World"), cancel);
 
             var consumer = client.NewConsumer()
                 .SubscriptionName("MySubscription")
-                .Topic(myTopic)
+                .Topic(topic)
                 .Create();
 
             await foreach (var message in consumer.Messages())
             {
                 Console.WriteLine("Received: " + Encoding.UTF8.GetString(message.Data.ToArray()));
-                await consumer.Acknowledge(message);
+                await consumer.Acknowledge(message, cancel);
             }
-            cts.Cancel();
         }
 
         private static async ValueTask Monitor(IProducer producer, CancellationToken cancellationToken)
